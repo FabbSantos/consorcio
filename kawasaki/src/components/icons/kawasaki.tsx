83e1047eaec0a70054f77e515cbfff042db24e3c
Fabrício Bahiense Santos <fabriciobs2000@gmail.com>
2022-12-28 15:55:20 -0300

    .

--- kawasaki/src/components/icons/kawasaki.tsx
+++ kawasaki/src/components/icons/kawasaki.tsx
@@ -1,5 +1,5 @@
 import logo from 'src/assets/img/logo_kawsaki-2.webp'
 
 export const KawaLogo = () => (
-  <img src={logo} alt="Kawasaki Logo" />
+  <img src={logo} alt="Kawasaki Logo" height="40px" width="150px" />
 );
